71e0bafd27503ad0150c41b26a946a1c4f5d7342
Menin-Colin <colin.menin@aol.com>
2025-02-10 17:36:37 +0100

    composants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./composants/**/*.{html,js}"
+  ],
   theme: {
-    
     extend: {
-      screens:{
-        'xs':'480px',
+      screens: {
+        'xs': '480px',
         'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
   plugins: [],
-  darkMode: 'media',
   darkMode: 'class',
 }
-
-
